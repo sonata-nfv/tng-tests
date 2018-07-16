16bff01abd49d74bef5e9dac71e3b4a8ffb9299a
alemanyp <paprats8@gmail.com>
2018-07-16 14:32:24 +0200

    functional test wit 2 of 4 steps working: Upload package and service --------> ok Create NetSlice Template ----------> ok Create NetSlice Instantiation -----> no Terminate NetSlice Instantiation --> no Delete NetSlice Template ----------> ok Delete Package and Service --------> ok

--- tests/SP/SP.func.1.createNST/step_1_valid_pkg_stored/Valid_package_is_stored.sh
+++ tests/SP/SP.func.1.createNST/step_1_valid_pkg_stored/Valid_package_is_stored.sh
@@ -2,33 +2,38 @@
 
 delete_sp=$(awk '/delete_sp/ {print $2}' envfile.yml)
 echo $delete_sp
-
 Clean=$(curl -X DELETE  ""$delete_sp"")
 
+echo "------Validate_1-----"
 upload=$(awk '/upload_package/ {print $2}' envfile.yml)
 echo $upload
+echo
 
-Result=$(curl -v -i -X POST  -F "package=@./eu.5gtango.ns-haproxy.0.1.tgo" ""$upload"")
-
+echo "------Validate_2-----"
+Result=$(curl -v -k -i -X POST  -F "package=@$package_name" ""$upload"")
 echo $Result
+echo
 
+echo "------Validate_3-----"
 package_process_uuid=$(printf "$Result" | grep -Po 'package_process_uuid":"\K[^"]+')
-echo
 echo $package_process_uuid
+echo
 
-sleep 15
+sleep 20
 
+echo "------Validate_4-----"
 status_url=$upload"/status/"$package_process_uuid
-echo
 echo $status_url
-
-status=$(curl ""$upload"/status/""$package_process_uuid")
 echo
+
+echo "------Validate_5-----"
+#status=$(curl ""$upload"/status/""$package_process_uuid")
+status=$(curl -k $status_url)
 echo $status
 echo
 
+echo "------Validate_6-----"
 package_id=$(printf "$status" | grep -Po 'package_id":"\K[^"]+')
-echo
 echo $package_id
 echo
 
