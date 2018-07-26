87749eded394b8bd19962d29f9ce0517779f0f69
luishens01 <luis.hensruiz.external@atos.net>
2018-07-26 11:55:15 +0200

    added instantiation of service to sp 7 test

--- tests/SP/SP.int.7/Instantiate_a_service.sh
+++ tests/SP/SP.int.7/Instantiate_a_service.sh
@@ -42,8 +42,29 @@ echo
 echo $monitoring
 echo
 
-delete_package=$(curl -X DELETE ""$upload"/""$package_id")
+#instantiating the NS
+instantiate=$(awk '/instantiate_ns/ {print $2}' envfile.yml)
+echo $instantiate
+
+services=$(awk '/services_host/ {print $2}' envfile.yml)
+echo $services
 echo
-echo $delete_package
+ns_uuid=$(curl  ""$services"" | jq '.[0].uuid')
 echo
+echo "the ns uuid is: "$ns_uuid
+echo
+requesting="curl -v -i -H content-type:application/json -X POST ""$instantiate""  -d {"\"service_uuid"\":""$ns_uuid""}"
+echo
+echo "this is the requesting curl:" 
+echo $requesting
+echo
+#instantiating=$(curl -v -i -H content-type:application/json -X POST ""$instantiate""  -d '{"\"service_uuid"\":""$ns_uuid""}')
+instantiating=$($requesting)
+echo $instantiating
+
+
+#delete_package=$(curl -X DELETE ""$upload"/""$package_id")
+#echo
+#echo $delete_package
+#echo
 
