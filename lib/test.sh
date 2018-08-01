520fe1d055a805386c00240cb2288d634f9c9a07
luishens01 <luis.hensruiz.external@atos.net>
2018-08-01 09:48:21 +0200

    added functions file

---
+++ lib/test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+unset env
+ ## loading environment
+. ./envfile
+logger "This is your selected environment:  $env"
+
+## importing the functions file
+. ./functions.lib
+
+envfile=$1
+#echo $envfile
+
+
+if [ -z "$1" ] 
+then
+	echo
+	logger "Invalid environment for this test script"		
+	exit 1
+	
+else	
+
+
+
+	getPackages "$env" 
+	uploadPackage "$env" "5gtango-ns-package-example.tgo"
+
+fi
+
+
+
+
+
