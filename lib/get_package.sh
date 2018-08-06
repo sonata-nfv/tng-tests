f6bfc1d45c7a7611e2612fa941c2687661ffae58
luishens01 <luis.hensruiz.external@atos.net>
2018-08-06 12:50:23 +0200

    Modify get_package script example

--- lib/get_package.sh
+++ lib/get_package.sh
@@ -1,36 +1,21 @@
 #!/bin/bash
 
-#unset env
- ## loading environment
-##. ./envfile
-
-
 ## importing the functions file
+
 . ./functions.lib
 
-envfile=$1
-#echo $envfile
-
-
 ## Setting the environment
-#unset env
+
 setEnvironment "$1"
 logger "This is your selected environment:  $env"
 
 
+## Executing get package and showing some values
 
+getPackage "$env" "ns-package-example" "0.1" "eu.5gtango"
+echo "Pkg uuid: "$Package_uuid
+echo "Pkg Staus: "$Package_status
 
-if [ -z "$1" ] 
-then
-	echo
-	logger "Invalid environment for this test script"		
-	exit 1
-	
-else	
-	getPackage "$env" "ns-package-example" "0.1" "eu.5gtango"
-	echo $Package_uuid
-	echo $Package_status
-fi
 
 
 
