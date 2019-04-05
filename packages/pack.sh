7968b8ef671ec57483988a36ba5946778c829ad9
peusterm <manuel.peuster@uni-paderborn.de>
2019-04-05 14:24:23 +0200

    Added NSID2V #30
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

--- packages/pack.sh
+++ packages/pack.sh
@@ -2,6 +2,8 @@
 set -e
 # directly call the validator (to make error pasing simple)
 tng-validate --project NSID1V -t
+tng-validate --project NSID2V -t
 
 # package all projects
 tng-pkg -p NSID1V --skip-validation
+tng-pkg -p NSID2V --skip-validation
