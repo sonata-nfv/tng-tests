1ec4e8ebae30ca8c78178e81aaac318b9c57285b
aureliano sinatra <sinaure@gmail.com>
2019-05-20 17:14:48 +0200

    removed entrypoint2

--- tests/VnV/probes/jmeter/entrypoint.sh
+++ tests/VnV/probes/jmeter/entrypoint.sh
@@ -2,6 +2,5 @@
 
 source /app/config.cfg
 
-jmeter -n -Jip=$IP -Jport=$PORT -t /app/input_simulator.jmx -l results.jtl -j jmeter.log
+jmeter -n -Jip=$IP -Jport=$PORT -t /app/input_simulator.jmx -l /app/results.jtl -j /app/jmeter.log
 
-# Keep entrypoint simple: we must pass the standard JMeter arguments
