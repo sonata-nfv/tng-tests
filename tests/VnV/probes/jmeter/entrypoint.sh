08cfa0d7d7380a77d9a7afd2b9a80d08362201a0
aureliano sinatra <sinaure@gmail.com>
2019-05-23 11:02:23 +0200

    externalised jmeter conf

--- tests/VnV/probes/jmeter/entrypoint.sh
+++ tests/VnV/probes/jmeter/entrypoint.sh
@@ -2,5 +2,8 @@
 
 source /app/config.cfg
 
-jmeter -n -Jip=$IP -Jport=$PORT -t /app/input_simulator.jmx 
+echo "threads = $THREADS"
+echo "DURATION = $DURATION"
+echo "RAMP_TIME = $RAMP_TIME"
+jmeter -n -Jip=$IP -Jport=$PORT -Jthreads=$THREADS -Jduration=$DURATION -Jramp_time=$RAMP_TIME -t /app/input_simulator.jmx -l /app/results/out.jtl
 
