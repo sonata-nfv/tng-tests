7c8b556f61f4050c9ec3804976906c96d5dbf033
aureliano sinatra <sinaure@gmail.com>
2019-06-07 12:18:51 +0200

    added mqtt probe

--- tests/VnV/probes/mqtt/entrypoint.sh
+++ tests/VnV/probes/mqtt/entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+source /mqtt-test/config.cfg
 
-python3 mqttgen.py config.json
+echo "ip = $IP"
+echo "port = $PORT"
+echo "packet size = $SIZE"
+echo "messages per client = $COUNT"
+echo "clients = $CLIENTS"
+
+
+mqtt-benchmark --broker tcp://$IP:$PORT --count $COUNT --size $SIZE --clients $CLIENTS --qos 2 --format json --username $USERNAME --password $PASSWORD
 
