c3a510f4a2a1aea2504c18d77768076e2735faf1
aureliano sinatra <sinaure@gmail.com>
2019-06-13 14:49:39 +0200

    added project to pack.sh

--- packages/pack.sh
+++ packages/pack.sh
@@ -5,6 +5,8 @@ tng-validate --project NSID1V -t
 tng-validate --project NSID2V -t
 tng-validate --project NSID1C -t
 tng-validate --project NSID2C -t
+tng-validate --project NSINDP1C -t
+
 
 # package all projects
 tng-pkg -p NSID1V --skip-validation
@@ -13,6 +15,8 @@ tng-pkg -p NSID1C --skip-validation
 tng-pkg -p NSID2C --skip-validation
 tng-pkg -p NSID1V_OSM --skip-validation
 tng-pkg -p NSIMPSP --skip-validation
+tng-pkg -p NSINDP1C --skip-validation
 tng-pkg -p TSTIMPSP --skip-validation
 tng-pkg -p NSID1V_cirros_OSM --skip-validation
 tng-pkg -p TSTPING --skip-validation
+tng-pkg -p TSTMQTT --skip-validation
