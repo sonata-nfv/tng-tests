9e99ca38a419bf1f4644fa6b76a926b3c210c548
Stefan Schneider <stefan.schneider@upb.de>
2019-07-03 10:31:39 +0200

    create result csv based on current time to avoid overriding

--- tests/SDK/test_sdk01_project_management/test.sh
+++ tests/SDK/test_sdk01_project_management/test.sh
@@ -5,12 +5,13 @@
 # - install tng-sdk-project (and activate the venv)
 # - delete all projects in the "projects" folder (not the folder itself)
 
-printf "Running test_sdk01_project_management; writing results to results.csv"
-printf "runtime(s),max memory(kb)\n" >> "results/results.csv"
+FILE="results/$( date +%Y-%m-%d_%H-%M-%S ).csv"
+printf "Running test_sdk01_project_management\n"
+printf "runtime(s),max memory(kb)\n" >> $FILE
 for i in `seq 1 2`; do
 	cmd="tng-project -p "projects/test_prj${i}" --vnfs $i"
 	printf "${cmd}\n"
 	# record time (s) and max memory (kb) and save to file (append)
-	/usr/bin/time --output='results/results.csv' -a --format='%U,%M' --quiet ${cmd}
+	/usr/bin/time --output=$FILE -a --format='%U,%M' --quiet ${cmd}
 done
-printf "Done"
+printf "Done. Results in ${FILE}\n"
