7a65501232a86715bd84cb5bc244db99d3a938e1
Stefan Schneider <stefan.schneider@upb.de>
2019-07-04 09:56:36 +0200

    added test script for 30x 100 runs

---
+++ tests/SDK/test_sdk01_project_management/test_30x100.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# test: create projects with increasing num of VNFs and measure performance
+# prerequisit: 
+# - install tng-sdk-project (and activate the venv)
+# - delete all projects in the "projects" folder (not the folder itself)
+
+FILE="results/$( date +%Y-%m-%d_%H-%M-%S ).csv"
+printf "Running test_sdk01_project_management\n"
+printf "Runtime (s),Max memory (kb)\n" >> $FILE
+for j in `seq 1 30`; do
+	for i in `seq 1 100`; do
+		cmd="tng-project -p "projects/test_prj${i}" --vnfs $i"
+		printf "${cmd}\n"
+		# record time (s) and max memory (kb) and save to file (append)
+		/usr/bin/time --output=$FILE -a --format='%U,%M' --quiet ${cmd}
+	done
+done
+printf "Done. Results in ${FILE}\n"
