fb02292e5456d1169c6816a56662e9a7dff9c340
Miguel <miguel.rivas@quobis.es>
2019-07-17 10:08:31 +0200

    Add the script for tng-sdk-validator tests and the first result files

---
+++ tests/SDK/test_sdk03_validation/test_integrity_30x100.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FILE="results/integrity_$( date +%Y-%m-%d_%H-%M-%S ).csv"
+printf "Runtime (s),Max memory (kb)\n" >> $FILE
+for j in `seq 1 30`; do
+	for i in `seq 1 100`; do
+		printf "Running i:${i} j:${j}\n"
+		cmd="tng-sdk-validate -i --project "./project_${i} #path to projects folder
+		printf "${cmd} j: ${j} i: ${i}\n"
+		/usr/bin/time --output=$FILE -a --format='%U,%M' --quiet ${cmd}
+	done
+done
+printf "Done. Results in ${FILE}\n"
